4d352c67c513b55c082c02a341cee3acd3fe33cf
Bruno Gabriel Rambo <brunogaar2@gmail.com>
2024-08-14 00:44:40 -0300

    FIX Html and Css code

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,7 @@
 const openButtons = document.querySelectorAll('.open-modal');
 
 openButtons.forEach(button => {
-    button.addEventListener('click , () =>'{
+    button.addEventListener('click' , () =>{
         const modalId = button.getAttribute ('data-modal');
         console.log(modalId);
         
