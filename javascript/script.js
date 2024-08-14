1d2210a2a0a930ddc4e8ca940ed8c1a4de6d2779
Bruno Gabriel Rambo <brunogaar2@gmail.com>
2024-08-14 00:27:51 -0300

    Starting the development of a basic modal
    Continuing my journey with HTML, CSS, and JavaScript

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+const openButtons = document.querySelectorAll('.open-modal');
+
+openButtons.forEach(button => {
+    button.addEventListener('click , () =>'{
+        const modalId = button.getAttribute ('data-modal');
+        console.log(modalId);
+        
+    });
+});
