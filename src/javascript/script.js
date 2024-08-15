765d1341207c888a6c103f9b3b5498f64427d2da
Bruno Gabriel Rambo <brunogaar2@gmail.com>
2024-08-15 04:25:34 -0300

    Close to finalizing my modal project

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,7 +3,20 @@ const openButtons = document.querySelectorAll('.open-modal');
 openButtons.forEach(button => {
     button.addEventListener('click' , () =>{
         const modalId = button.getAttribute ('data-modal');
-        console.log(modalId);
+        const modal= document.getElementById(modalId);
         
+        
+        modal.showModal(); 
     });
 });
+
+const closeButtons = document.querySelectorAll('.close-modal');
+
+closeButtons .foreach(button=> {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute ('data-modal')
+        const modal= document.getElementById(modalId);
+
+        modal.closest
+    })
+})
